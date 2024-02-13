d3765cb31a4ec7591773defd5c548d2acac6c19d
Jojo-Programming <mpagijollyuganda@gmail.com>
2024-02-13 00:50:41 -0800

    corrections

--- models/base_model.py
+++ models/base_model.py
@@ -11,17 +11,17 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialises the BaseModel class"""
     if kwargs:
-    for key, val in kwargs.items():
-    if key != '__class__':
-    if key == 'created_at' or key == 'updated_at':
-    setattr(self, key, datetime.datetime.fromisoformat(val))
-        else:
-            setattr(self, key, val)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-            models.storage.new(self)
+        for key, val in kwargs.items():
+            if key != '__class__':
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.datetime.fromisoformat(val))
+            else:
+                setattr(self, key, val)
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.datetime.now()
+                    self.updated_at = datetime.datetime.now()
+                    models.storage.new(self)
 
     def __str__(self):
         """Return the string representation of instance"""
